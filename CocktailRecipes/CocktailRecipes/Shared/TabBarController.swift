5ba08c627236a26bb01f6baac8f0f7143183e827
Eric Rado <erado@axxess.com>
2020-07-20 15:09:52 -0500

    Setup tab bar controller with view controllers and tab bar items with their corresponding images.

---
+++ CocktailRecipes/CocktailRecipes/Shared/TabBarController.swift
@@ -0,0 +1,42 @@
+//
+//  TabBarController.swift
+//  CocktailRecipes
+//
+//  Created by Eric Rado on 7/20/20.
+//  Copyright © 2020 Eric Rado. All rights reserved.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		view.backgroundColor = .white
+
+		let homeViewController = HomeViewController()
+		homeViewController.tabBarItem.image = UIImage(named: "home")
+		homeViewController.tabBarItem.title = ""
+
+		let drinksViewController = DrinksViewController()
+		drinksViewController.tabBarItem.image = UIImage(named: "cocktail")
+		drinksViewController.tabBarItem.title = ""
+
+		let ingredientsViewController = IngredientsViewController()
+		ingredientsViewController.tabBarItem.image = UIImage(named: "ingredient")
+		ingredientsViewController.tabBarItem.title = ""
+
+		let savedDrinksViewController = SavedDrinksViewController()
+		savedDrinksViewController.tabBarItem.image = UIImage(named: "bookmark")
+		savedDrinksViewController.tabBarItem.title = ""
+
+		setViewControllers([
+			homeViewController,
+			drinksViewController,
+			ingredientsViewController,
+			savedDrinksViewController
+			], animated: true)
+		selectedIndex = 0
+    }
+
+}
