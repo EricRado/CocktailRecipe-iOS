9d19ee3b399cc996df801644a17ad0555bf4ecdc
Eric Rado <eradowado@gmail.com>
2020-07-24 11:56:29 -0500

    Added extension for UICollectionReusableView

---
+++ CocktailRecipes/CocktailRecipes/Extensions/UICollectionViewCell+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UICollectionViewCell+Extension.swift
+//  CocktailRecipes
+//
+//  Created by Eric Rado on 7/21/20.
+//  Copyright © 2020 Eric Rado. All rights reserved.
+//
+
+import UIKit
+
+extension UICollectionReusableView {
+	static var identifier: String {
+		return String(describing: self)
+	}
+}
